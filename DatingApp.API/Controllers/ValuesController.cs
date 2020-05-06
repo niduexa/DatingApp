c3072f119afe26cef05e6a4af03a5e77f982fee8
Chamal <niduexa@gmail.com>
2020-05-07 01:26:34 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[EnableCors("AllowOrigin")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+           "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        }; 
+
+        // private readonly ILogger<ValuesController> _logger;
+
+        // public ValuesController(ILogger<ValuesController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // { 
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // } 
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            //return Ok();
+           var values = _context.Values.ToList();
+           return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
